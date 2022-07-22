2ea1fb53e46f0831fa5138c4c3281b05f3e0610c
lukaofirst <lucasmacedo123@gmail.com>
2022-07-22 17:53:49 -0300

    creating routes to login, register user and go to profile

---
+++ WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using Core.Entities;
+using Core.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly IJWTManagerService _jwtManagerService;
+        private readonly IUserService _userService;
+
+        public AccountController(IJWTManagerService jwtManagerService, IUserService userService)
+        {
+            _jwtManagerService = jwtManagerService;
+            _userService = userService;
+        }
+
+        [Authorize]
+        [HttpGet("Profile")]
+        public async Task<IActionResult> Get()
+        {
+            var users = await _userService.GetAll();
+
+            return Ok(users);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(User user)
+        {
+            var token = await _jwtManagerService.Authenticate(user);
+
+            if (token == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(token);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(User user)
+        {
+            var emailExist = await _userService.FindByEmail(user.email!);
+
+            if(emailExist != null) return BadRequest("Email already exist!");
+
+            var entity = await _userService.InsertOne(user);
+
+            return Ok(entity);
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,11 +1,37 @@
 using Core.Settings;
 using IoC;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.Configure<SettingsConfig>(builder.Configuration.GetSection("SettingsConfig"));
+builder.Services.Configure<JWTConfig>(builder.Configuration.GetSection("JWTConfig"));
 builder.Services.AddMvc().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNameCaseInsensitive = true);
+
+// Authentication
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    var key = Encoding.UTF8.GetBytes(builder.Configuration["JWTConfig:Key"]);
+    o.SaveToken = true;
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidAudience = builder.Configuration["JWT:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+    };
+});
+
 
 DependencyContainer.StartServices(builder.Services);
 
@@ -24,6 +50,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
