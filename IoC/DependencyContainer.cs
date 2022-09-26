0aba35ec1654d2d8773c382c6ddef5d839c9934e
lukaofirst <lucasmacedo123@gmail.com>
2022-09-25 21:06:18 -0300

    adding AutoMapper and extracting JWT setup from Program.cs

--- IoC/DependencyContainer.cs
+++ IoC/DependencyContainer.cs
@@ -1,18 +1,27 @@
-﻿using Core.Interfaces.Repositories;
-using Core.Interfaces.Services;
+﻿using Application.Interfaces.Services;
+using Application.Mappings;
+using Application.Services;
+using Core.Interfaces.Repositories;
 using Infraestructure.Data;
 using Infraestructure.Repositories;
-using Infraestructure.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace IoC
 {
     public class DependencyContainer
     {
-        public static void StartServices(IServiceCollection services)
+        public static void StartServices(IServiceCollection services, IConfigurationRoot appSettings)
         {
             // Data Context
             services.AddSingleton<MongoDBContext>();
+
+            // AutoMapper
+            services.AddAutoMapper(typeof(MappingProfile));
 
             // Repositories
             services.AddScoped<IUserRepository, UserRepository>();
@@ -20,6 +29,28 @@ namespace IoC
             // Services
             services.AddScoped<IJWTManagerService, JWTManagerService>();
             services.AddScoped<IUserService, UserService>();
+
+
+            // JWT Authentication
+            services.AddAuthentication(x =>
+             {
+                 x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                 x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+             }).AddJwtBearer(o =>
+             {
+                 var key = Encoding.UTF8.GetBytes(appSettings.GetValue<string>("JWTConfig:Key"));
+                 o.SaveToken = true;
+                 o.TokenValidationParameters = new TokenValidationParameters
+                 {
+                     ValidateIssuer = false,
+                     ValidateAudience = false,
+                     ValidateLifetime = true,
+                     ValidateIssuerSigningKey = true,
+                     ValidIssuer = appSettings.GetValue<string>("JWT:Issuer"),
+                     ValidAudience = appSettings.GetValue<string>("JWT:Audience"),
+                     IssuerSigningKey = new SymmetricSecurityKey(key),
+                 };
+             });
         }
     }
 }
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,8 +1,5 @@
 using Core.Settings;
 using IoC;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,29 +8,8 @@ builder.Services.Configure<SettingsConfig>(builder.Configuration.GetSection("Set
 builder.Services.Configure<JWTConfig>(builder.Configuration.GetSection("JWTConfig"));
 builder.Services.AddMvc().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNameCaseInsensitive = true);
 
-// Authentication
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(o =>
-{
-    var key = Encoding.UTF8.GetBytes(builder.Configuration["JWTConfig:Key"]);
-    o.SaveToken = true;
-    o.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["JWT:Issuer"],
-        ValidAudience = builder.Configuration["JWT:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-    };
-});
-
-
-DependencyContainer.StartServices(builder.Services);
+var appSettings = builder.Configuration.AddJsonFile("appsettings.json").Build();
+DependencyContainer.StartServices(builder.Services, appSettings);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
